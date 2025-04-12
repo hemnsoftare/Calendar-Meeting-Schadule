bf533cadbb14dcaa3ee1c27dd57a39d9732d6030
hemnsoftare <138198540+hemnsoftare@users.noreply.github.com>
2025-04-12 12:24:05 +0300

    add evenet and show

--- app/actions.ts
+++ app/actions.ts
@@ -2,9 +2,14 @@
 import { parseWithZod } from "@conform-to/zod";
 import prisma from "./lib/db";
 import { requireUser } from "./lib/hook";
-import { onboardingSchemaValidation, settingsSchema } from "./lib/zodSchemes";
+import {
+  eventTypeSchema,
+  onboardingSchemaValidation,
+  settingsSchema,
+} from "./lib/zodSchemes";
 import { Presentation } from "lucide-react";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 export const onBoardingActions = async (
   prevState: unknown,
   formData: FormData
@@ -35,8 +40,56 @@ export const onBoardingActions = async (
     data: {
       userName: submission.value.username,
       name: submission.value.fullname,
+      availability: {
+        create: [
+          // Use 'create' for nested create operations, not 'createMany'
+          {
+            day: "Friday",
+            fromTime: "09:00",
+            tillTime: "17:00",
+            isActive: true,
+          },
+          {
+            day: "Wednesday",
+            fromTime: "09:00",
+            tillTime: "17:00",
+            isActive: true,
+          },
+          {
+            day: "Monday",
+            fromTime: "09:00",
+            tillTime: "17:00",
+            isActive: true,
+          },
+          {
+            day: "Tuesday",
+            fromTime: "09:00",
+            tillTime: "17:00",
+            isActive: true,
+          },
+          {
+            day: "Thursday",
+            fromTime: "09:00",
+            tillTime: "17:00",
+            isActive: true,
+          },
+          {
+            day: "Saturday",
+            fromTime: "09:00",
+            tillTime: "17:00",
+            isActive: true,
+          },
+          {
+            day: "Sunday",
+            fromTime: "09:00",
+            tillTime: "17:00",
+            isActive: true,
+          },
+        ],
+      },
     },
   });
+
   redirect("/onboarding/grant-id");
 };
 
@@ -56,6 +109,67 @@ export const settingsActions = async (
     data: {
       name: submission.value.fullname,
       image: submission.value.profileImage,
+    },
+  });
+  redirect("/dashboard");
+};
+
+export async function updateAvailabilityAction(formData: FormData) {
+  // const session = await requireUser();
+  console.log(formData);
+  const rawData = Object.fromEntries(formData.entries());
+  const availabilityData = Object.keys(rawData)
+    .filter((key) => key.startsWith("id-"))
+    .map((key) => {
+      const id = key.replace("id-", "");
+      return {
+        id,
+        isActive: rawData[`isActive-${id}`] === "on",
+        fromTime: rawData[`fromTime-${id}`] as string,
+        tillTime: rawData[`tillTime-${id}`] as string,
+      };
+    });
+
+  try {
+    await prisma.$transaction(
+      availabilityData.map((item) =>
+        prisma.availability.update({
+          where: { id: item.id },
+          data: {
+            isActive: item.isActive,
+            fromTime: item.fromTime,
+            tillTime: item.tillTime,
+          },
+        })
+      )
+    );
+
+    revalidatePath("/dashboard/availability");
+    // return { status: "success", message: "Availability updated successfully" };
+  } catch (error) {
+    console.error("Error updating availability:", error);
+    // return { status: "error", message: "Failed to update availability" };
+  }
+}
+
+export const CreateEventTypeAction = async (
+  prvent: unknown,
+  formData: FormData
+) => {
+  console.log(prvent);
+  const session = await requireUser();
+  const data = parseWithZod(formData, {
+    schema: eventTypeSchema,
+  });
+  if (data.status !== "success") return data.reply();
+  await prisma.eventType.create({
+    data: {
+      url: data.value.url,
+      title: data.value.title,
+      description: data.value.description,
+      duration: data.value.duration,
+      videoCallSoftware: data.value.videoCallSoftware,
+      userId: session.user?.id,
     },
   });
   redirect("/dashboard");
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,149 @@
 import React from "react";
-import { redirect } from "next/navigation";
-import { signOut } from "../lib/auth";
-
+import { notFound } from "next/navigation";
+import { requireUser } from "../lib/hook";
+import prisma from "../lib/db";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { EmptyState } from "../components/dashboard/EmptyState";
+import {
+  ExternalLink,
+  Link2,
+  MoreVertical,
+  Pen,
+  Settings,
+  Trash,
+  User2,
+} from "lucide-react";
+import { Switch } from "@/components/ui/switch";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+const getData = async (userId: string) => {
+  const data = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      id: true,
+      userName: true,
+      EventType: {
+        select: {
+          id: true,
+          title: true,
+          description: true,
+          duration: true,
+          url: true,
+          videoCallSoftware: true,
+          isActive: true,
+          eventPrice: true,
+          User: {
+            select: {
+              userName: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  if (!data) return notFound();
+  return data;
+};
 const page = async () => {
+  const user = await requireUser();
+  const data = await getData(user.user?.id || "");
   return (
-    <div>
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-          redirect("/");
-        }}
-      >
-        <button className="bg-red-500 text-white px-4 py-2 rounded">
-          Logout
-        </button>
-      </form>
-      <h1>dashboade in here </h1>
-    </div>
+    <>
+      {" "}
+      <div className="flex items-center justify-between px-2">
+        <div className="sm:grid gap-1 hidden">
+          <h1 className="font-heading text-3xl md:text-4xl">Event Types</h1>
+          <p className="text-lg text-muted-foreground">
+            Create and manage your event types.
+          </p>
+        </div>
+        <Button asChild>
+          <Link href="/dashboard/new">Create New Event</Link>
+        </Button>
+      </div>
+      {data.EventType.length === 0 ? (
+        <EmptyState
+          title="You have no Event Types"
+          description="You can create your first event type by clicking the button below."
+          buttonText="Add Event Type"
+          href="/dashboard/new"
+        />
+      ) : (
+        <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+          {data.EventType.map((event) => (
+            <div
+              key={event.id}
+              className="overflow-hidden shadow rounded-lg relative"
+            >
+              <div className="absolute top-2 right-2 ">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size={"icon"}>
+                      <Settings className="h-4 w-4" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Event</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                      <Link href={`/${event.User?.userName}/${event.url}`}>
+                        <ExternalLink className="mr-2 size-4" />
+                        Preview
+                      </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Link href={`/${event.User?.userName}/${event.url}`}>
+                        <Link2 className="mr-2 size-4" />
+                        Copy
+                      </Link>
+                    </DropdownMenuItem>{" "}
+                    <DropdownMenuItem>
+                      <Link href={`/${event.User?.userName}/${event.url}`}>
+                        <Pen className="mr-2 size-4" />
+                        Edit
+                      </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                      <Link href={`/${event.User?.userName}/${event.url}`}>
+                        <Trash className="mr-2 size-4" />
+                        Delete
+                      </Link>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+              <Link href={"/"} className="flex items-center p-5">
+                <div className="shrink-0">
+                  <User2 className="s0ze-6" />
+                </div>
+                <div className="ml-5 w-0 flex-1">
+                  <dl>
+                    <dt className="text-sm font-medium text-muted-foreground">
+                      {event.duration} musnise meeting
+                    </dt>
+                    <dd className="text-lg font-medium">{event.title}</dd>
+                  </dl>
+                </div>
+              </Link>
+              <div className="bg-muted px-5 py-2 flex items-center justify-between">
+                <Switch />
+                <Button> Edit Event</Button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
